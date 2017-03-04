ddb074e2f37da9ef2fbb9561d8d657ca05c55a19
canlas1 <dc@uscr.com>
2017-03-03 19:39:08 -0500

    push2

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Psychic-Game
-Homework 3 Easier
+# Hangman-Game
+Hangman-Game Homework 3
---
+++ assets/javascript/game.js
@@ -0,0 +1,81 @@
+//set global varibles
+
+var alphabetArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft =9;
+var guessedLetterArr = [];
+var letterToGuess = null;
+
+//computer selects a random letter from the available choices
+var computerGuesses = alphabetArr[Math.floor(Math.random() * alphabetArr.length)];
+ console.log(computerGuesses);
+
+//function that allows 9 total guesses
+
+var newGuessesLeft = function(){ //insert HTML from class .guessLeft HTLM replaces then add the variable guessesLeft
+	document.querySelector(".guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;		
+};
+	console.log(newGuessesLeft);
+
+var newGuessesSoFar = function(){ //insert HTML from class .soFar HTML replaces then join the string of the guessedLetterArr separate by a comma
+	document.querySelector(".soFar").innerHTML = "Your Guesses So Far: " + guessedLetterArr.join(",");
+};
+	console.log(newGuessesSoFar);
+
+var newLetterToGuess = function(){
+	this.letterToGuess = this.alphabetArr[Math.floor(Math.random()* this.alphabetArr.length)];
+};
+  console.log(newLetterToGuess);
+
+
+//function to reset everything
+var reset = function() {
+  guesses = 9;
+  guessesLeft = 9;
+  guessedLetterArr = [];
+
+  newGuessesLeft();
+  newGuessesSoFar();
+  newLetterToGuess();
+}
+
+	newLetterToGuess();
+	newGuessesLeft();
+
+
+//key starts the game
+document.onkeyup = function(event) { //function that goes all the way to the end of the document
+    guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); // lowercase/uppercase function
+
+  guessedLetterArr.push(userGuess);
+  newGuessesLeft();
+  newGuessesSoFar();
+
+        if (guessesLeft > 0){
+            if (userGuess === letterToGuess){
+                wins++;
+
+                document.querySelector(".wins").innerHTML = "Wins: " + wins;
+                console.log("This is working!");
+                //call the reset
+                reset();
+            }
+
+        }else if(guessesLeft === 0){
+            // Then we will loss and we'll update the html to display the loss 
+            losses++;
+            document.querySelector(".losses").innerHTML = "Losses: " + losses;
+	            console.log("you are losing");
+	            // Then we'll call the reset. 
+            reset();
+        }
+};
+
+
+
+
+
+
